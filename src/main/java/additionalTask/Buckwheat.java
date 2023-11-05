15a03e734c49e4b003626175e2359506a6869601
ephii0p <belunkind@gmail.com>
2023-11-05 20:18:50 +0400

    commit

--- src/main/java/additionalTask/Buckwheat.java
+++ src/main/java/additionalTask/Buckwheat.java
@@ -9,14 +9,14 @@ public class Buckwheat {
         int buckwheatPerMonth = 6;
         int buckwheatWeight = months * buckwheatPerMonth;
 
-        int[] calculations = new int[5];
+        int[] calculations = new int[6];
         calculations[0] = buckwheatWeight * rentPerKg;
         calculations[1] = (buckwheatWeight - buckwheatPerMonth) * rentPerKg;
         calculations[2] = calculations[1] - (buckwheatPerMonth * rentPerKg);
         calculations[3] = calculations[2] - (buckwheatPerMonth * rentPerKg);
         calculations[4] = calculations[3] - (buckwheatPerMonth * rentPerKg);
-
-        for (int i = 0; i < 5; i++) {
+        calculations[5] = calculations[4]- (buckwheatPerMonth * rentPerKg);
+        for (int i = 0; i < 6; i++) {
             System.out.println(calculations[i]);
         }
     }
