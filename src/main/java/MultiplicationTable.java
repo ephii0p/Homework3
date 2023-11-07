711edea4599382845a241d457520db7d7abb74a9
ephii0p <belunkind@gmail.com>
2023-11-07 10:01:37 +0400

    commit

--- src/main/java/MultiplicationTable.java
+++ src/main/java/MultiplicationTable.java
@@ -1,8 +1,8 @@
 public class MultiplicationTable {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.printf("%5d", (i + 1) * (j + 1));
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%5d", (i * j));
             }
             System.out.println("");
         }
